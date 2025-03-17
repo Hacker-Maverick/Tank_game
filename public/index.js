96cbd671eab2ae71a9e6306fa48def8d18bdeebd
Saurav Sagar <bigsagar18@gmail.com>
2025-03-17 07:31:22 +0530

    Tank game completed

---
+++ public/index.js
@@ -0,0 +1,265 @@
+const socket = io("http://localhost:3000");
+
+// Variables
+let submit = { username: null, roomid: null };
+let gameobj, obj, socketid;
+let bullet = [];
+
+
+// Event Listeners
+document.getElementById("create").addEventListener("click", () => {
+
+    document.getElementById("createjoin").style.display = "none";
+    document.getElementById("inputfields").style.display = "flex";
+    submit.username = document.getElementById("username").value;
+    document.getElementById("createbutton").style.display = "flex";
+
+})
+
+document.getElementById("join").addEventListener("click", async () => {
+    document.getElementById("createjoin").style.display = "none";
+    document.getElementById("inputfields").style.display = "flex";
+    document.getElementById("roominput").style.display = "flex";
+    document.getElementById("joinbutton").style.display = "flex";
+
+})
+
+document.getElementById("createbutton").addEventListener("click", () => {
+    if (document.getElementById("username").value) {
+        submit.username = document.getElementById("username").value;
+        submit.roomid = null;
+        socket.emit("createroom", submit);
+    }
+    else {
+        alert("Please enter a username")
+    }
+})
+
+document.getElementById("joinbutton").addEventListener("click", () => {
+    if (document.getElementById("username").value && document.getElementById("roomid").value) {
+        submit.username = document.getElementById("username").value;
+        submit.roomid = parseInt(document.getElementById("roomid").value);
+        socket.emit("joinroom", submit);
+    }
+    else {
+        alert("Please enter a username and valid Room-ID")
+    }
+})
+
+document.getElementById("start").addEventListener("click", () => {
+    socket.emit("start", submit.roomid)
+})
+
+// Getting socket data
+socket.on("joinroom", (data) => {
+    if (data.err) {
+        alert(data.err);
+    }
+    else {
+        document.getElementById("inputfields").style.display = "none";
+        document.getElementById("roominput").style.display = "none";
+        document.getElementById("joinbutton").style.display = "none";
+        document.getElementById("created").style.display = "flex";
+        document.getElementById("roomjoined").style.display = "flex";
+        document.getElementById("roomjoined").innerHTML = `<div>Room joined successfully.</div><div>Room ID : ${data.roomid}</div>`
+        socketid = data.userid
+    }
+})
+
+socket.on("createroom", (data) => {
+    document.getElementById("inputfields").style.display = "none";
+    document.getElementById("createbutton").style.display = "none";
+    document.getElementById("created").style.display = "flex";
+    document.getElementById("roomoutput").style.display = "flex";
+    document.getElementById("display").innerHTML = `<div>Room created successfully.</div><div>Room ID : ${data.roomid}</div>`
+    socketid = data.userid
+    submit.roomid = data.roomid;
+})
+
+socket.on("started", () => {
+    document.body.innerHTML = `<div id='scorecard' style='height:10vh; display:flex;'></div><div style='display:flex; justify-content:center;'><div id='playzone' style='height:90vh; width:90vh; position:relative; background-image:url("bg.jpg");'></div></div>`;
+    document.body.style.backgroundImage = 'url("images.jpeg")';
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize ="100% 100%" 
+
+    for (let i = 0; i < gameobj.length; i++) {
+        document.getElementById("scorecard").innerHTML += `<div style="width:100%;">
+        <div id="${i}username" style="height: 3vh; margin: 1vh; margin-bottom:0vh;"><strong style="text-shadow:1px 1px 2px white">${gameobj[i].username}</strong></div>
+        <div id="${i}hpdiv" style="height: 4vh; border: 2px solid black; margin: 1vh">
+        <div id="${i}hp" style="height: 100%; width:100%; background-color:blue;"></div></div></div>`
+    }
+
+    for (let i = 0; i < gameobj.length; i++) {
+        document.getElementById("playzone").innerHTML += `<div style="height:5vh; width:5vh; background-color:rgb(1, 151, 1); position: absolute; border-radius: 10px;" id="${i}">
+        <div id="noz${i}" style=" height: 1vh; width: 10vh; margin-top:2vh; margin-left: -2.5vh; ">
+            <div style="height: 1vh; width: 5vh; background-color: rgb(0, 72, 0); margin-left: 4.8vh; border-top-left-radius: 100px; border-bottom-left-radius: 100px;">
+            </div>
+        </div>
+    </div>`
+    }
+
+    // game updates
+    const getupdates = setInterval(() => {
+        socket.emit("updateserver", obj);
+    }, 20);
+
+    render();
+    controls();
+})
+
+socket.on("updateclient", (data) => {
+    gameobj = data;
+    obj = gameobj.find(obj => obj.userid === socketid);
+    checkhit();
+})
+
+socket.on("died", () => {
+    alert("youdied")
+    socket.emit("updateserver", obj)
+    socket.disconnect();
+    window.location.href="http://localhost:3000"
+})
+
+socket.on("hitconfirm", (index) => {
+    clearInterval(bullet[index]);
+    obj.bulletx[index] = -100;
+    obj.bullety[index] = -100;
+})
+
+socket.on("win", (data) => {alert(data);socket.disconnect();window.location.href="http://localhost:3000";})
+
+socket.on("deadtank", (index) => { document.getElementById(index).style.display = "none" })
+
+// Game logics
+function render() {
+    for (let i = 0; i < gameobj.length; i++) {
+        document.getElementById(`noz${i}`).style.transform = `rotate(${gameobj[i].noz}deg)`;
+        document.getElementById(`${i}`).style.top = `${gameobj[i].y}vh`;
+        document.getElementById(`${i}`).style.left = `${gameobj[i].x}vh`;
+        for (let j = 0; j < gameobj[i].bulletx.length; j++) {
+            if (gameobj[i].bullety[j] == -100) {
+                document.getElementById(i + "" + j).style.display="none"
+                document.getElementById(i + "" + j).style.display="none"
+            }
+            else if (document.getElementById(i + "" + j)) {
+                document.getElementById(i + "" + j).style.top = `${gameobj[i].bullety[j]}vh`;
+                document.getElementById(i + "" + j).style.left = `${gameobj[i].bulletx[j]}vh`;
+            }
+            else {
+                document.getElementById("playzone").innerHTML += `<div id="${i + "" + j}" style="background-color:red;height:1vh;width:1vh;border-radius:1vh;position:absolute;z-index:10;"></div>`
+            }
+        }
+    }
+    requestAnimationFrame(render);
+    renderhp();
+}
+
+function renderhp() {
+    for (let i = 0; i < gameobj.length; i++) {
+        document.getElementById(`${i}hp`).style.width = `${gameobj[i].hp}%`;
+    }
+}
+
+//controls
+function controls() {
+    let pressed = [null, null, null, null, null, null];
+
+    document.addEventListener("keydown", (e) => {
+        if (!pressed[0] && e.key == "w") {
+            pressed[0] = setInterval(() => {
+                if (obj.y > 0) { obj.y -= 0.25; }
+            }, 20);
+        }
+        if (!pressed[1] && e.key == "a") {
+            pressed[1] = setInterval(() => {
+                if (obj.x > 0) { obj.x -= 0.25; }
+            }, 20);
+        }
+        if (!pressed[2] && e.key == "s") {
+            pressed[2] = setInterval(() => {
+                if (obj.y < 85) { obj.y += 0.25; }
+            }, 20);
+        }
+        if (!pressed[3] && e.key == "d") {
+            pressed[3] = setInterval(() => {
+                if (obj.x < 85) { obj.x += 0.25; }
+            }, 20);
+        }
+        if (!pressed[4] && e.key == "ArrowLeft") {
+            pressed[4] = setInterval(() => {
+                obj.noz -= 2;
+            }, 20);
+        }
+        if (!pressed[5] && e.key == "ArrowRight") {
+            pressed[5] = setInterval(() => {
+                obj.noz += 2;
+            }, 20);
+        }
+    })
+    document.addEventListener("keyup", (e) => {
+        if (pressed[0] && e.key == "w") { clearInterval(pressed[0]); pressed[0] = null; }
+        if (pressed[1] && e.key == "a") { clearInterval(pressed[1]); pressed[1] = null; }
+        if (pressed[2] && e.key == "s") { clearInterval(pressed[2]); pressed[2] = null; }
+        if (pressed[3] && e.key == "d") { clearInterval(pressed[3]); pressed[3] = null; }
+        if (pressed[4] && e.key == "ArrowLeft") { clearInterval(pressed[4]); pressed[4] = null; }
+        if (pressed[5] && e.key == "ArrowRight") { clearInterval(pressed[5]); pressed[5] = null; }
+    })
+
+    document.addEventListener("keyup", async (e) => {
+        if (e.code == "Space") {
+            let initx = obj.x + 2,
+                inity = obj.y + 2,
+                index = obj.bulletx.length,
+                initnoz = obj.noz;
+
+            // Set initial bullet position
+            obj.bulletx[index] = initx;
+            obj.bullety[index] = inity;
+
+            // Convert angle to radians
+            let angleRad = initnoz * Math.PI / 180; // Subtract 90° to align with up direction
+
+            // Define bullet speed
+            let speed = 0.5;  // You can adjust speed as needed
+
+            // Calculate velocity components
+            let vx = speed * Math.cos(angleRad);
+            let vy = speed * Math.sin(angleRad);
+
+            // Move bullet
+            bullet.push(setInterval(() => {
+                obj.bulletx[index] += vx;  // Move in x direction
+                obj.bullety[index] += vy;  // Move in y direction (canvas inverted)
+
+                // Stop bullet if it goes out of bounds
+                if (obj.bulletx[index] < 0 || obj.bulletx[index] > 89 ||
+                    obj.bullety[index] < 0 || obj.bullety[index] > 89) {
+                    clearInterval(bullet[index]);
+                    obj.bulletx[index] = -100;
+                    obj.bullety[index] = -100;
+                }
+
+            }, 10));
+        }
+    });
+
+
+}
+
+let flagi, flagj;
+function checkhit() {
+
+    for (let i = 0; i < gameobj.length; i++) {
+        if (gameobj[i].userid == socketid) {
+            continue;
+        }
+        for (let j = 0; j < gameobj[i].bulletx.length; j++) {
+            if (flagi == i && flagj == j) { continue }
+            if ((obj.x < (gameobj[i].bulletx[j] + 0.5)) && ((obj.x + 5) > (gameobj[i].bulletx[j] + 0.5)) && (obj.y < (gameobj[i].bullety[j] + 0.5)) && ((obj.y + 5) > (gameobj[i].bullety[j] + 0.5))) {
+                obj.hp = obj.hp - 20;
+                flagi = i; flagj = j;
+                socket.emit("bullethit", { roomid: obj.roomid, i: i, j: j })
+            }
+        }
+    }
+}
---
+++ server.js
@@ -0,0 +1,153 @@
+import express from "express"
+import path from "path";
+import { Server } from "socket.io"
+import http from "http"
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Path
+const mypath = "D:/Git/tank"
+
+// Express Middlewares
+app.use(express.static("public"));
+
+// Express endpoints
+app.get("/", (req, res) => {
+    res.status(200).sendFile(path.join(mypath, "public", "index.html"));
+});
+
+app.get("/game", (req, res) => {
+    res.status(200).sendFile(path.join(mypath, "public", "game.html"));
+})
+
+// Server Listening
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
+
+//Variables 
+var count = 0;
+var gameobj = [];
+
+// Game logics
+function genroomid() {
+    count = count + 1;
+    return parseInt(Math.random() * 100000) + count * 100000
+}
+
+// Socket connections
+io.on("connection", (socket) => {
+
+    // Room create and join
+    socket.on("createroom", (submitdata) => {
+        submitdata.roomid = genroomid()
+        socket.join(submitdata.roomid);
+        //assigning values
+        let obj = {
+            roomid: null,
+            userid: null,
+            username: null,
+            x: null,
+            y: null,
+            hp: 100,
+            bulletx: [],
+            bullety: [],
+            noz: 0
+        }
+        obj.roomid = submitdata.roomid;
+        obj.userid = socket.id;
+        obj.username = submitdata.username;
+        obj.x = 25;
+        obj.y = 30;
+        gameobj[parseInt((submitdata.roomid) / 100000)] = [];
+        gameobj[parseInt((submitdata.roomid) / 100000)].push(obj);
+        console.log(`Socket ${socket.id} joined room ${submitdata.roomid}`);
+        submitdata.userid = socket.id;
+        socket.emit("createroom", submitdata);
+    })
+    socket.on("joinroom", (submitdata) => {
+        if (io.sockets.adapter.rooms.has(submitdata.roomid) && gameobj[parseInt((submitdata.roomid) / 100000)].length < 4) {
+            socket.join(submitdata.roomid);
+            let obj = {
+                roomid: null,
+                userid: null,
+                username: null,
+                x: null,
+                y: null,
+                hp: 100,
+                bulletx: [],
+                bullety: [],
+                noz: 0
+            }
+            obj.roomid = submitdata.roomid;
+            obj.userid = socket.id;
+            obj.username = submitdata.username;
+            if (gameobj[parseInt((submitdata.roomid) / 100000)].length == 1) {
+                obj.x = 65;
+                obj.y = 30;
+                obj.noz = 180
+            }
+            else if (gameobj[parseInt((submitdata.roomid) / 100000)].length == 2) {
+                obj.x = 25;
+                obj.y = 50;
+            }
+            else {
+                obj.x = 65;
+                obj.y = 50;
+                obj.noz = 180;
+            }
+            gameobj[parseInt((submitdata.roomid) / 100000)].push(obj);
+            console.log(`Socket ${socket.id} joined room ${submitdata.roomid}`);
+            submitdata.err = null
+            submitdata.userid = socket.id
+            socket.emit("joinroom", submitdata);
+        }
+        else if (!(io.sockets.adapter.rooms.has(submitdata.roomid))) {
+            submitdata.err = "Room does not exist.";
+            socket.emit("joinroom", submitdata);
+        }
+        else if (gameobj[parseInt((submitdata.roomid) / 100000)].length >= 4) {
+            submitdata.err = "Maximum players reached"
+            socket.emit("joinroom", submitdata)
+        }
+        else {
+            submitdata.err = "Some error occured. Try creating a new room."
+            socket.emit("joinroom", submitdata)
+        }
+    });
+    socket.on("start", (roomid) => {
+        io.to(roomid).emit("updateclient", gameobj[parseInt((roomid) / 100000)])
+        io.to(roomid).emit("started");
+        console.log(gameobj)
+    })
+
+    // Game Updates
+    socket.on("updateserver", (data) => {
+        if (gameobj[parseInt((data.roomid) / 100000)]) {
+            let index = gameobj[parseInt((data.roomid) / 100000)].findIndex(obj => obj.userid === data.userid)
+            gameobj[parseInt((data.roomid) / 100000)][index] = data;
+            socket.emit("updateclient", gameobj[parseInt((data.roomid) / 100000)])
+            checkdeath(data.roomid)
+        };
+    })
+
+    socket.on("bullethit", (data) => {
+        io.to(gameobj[parseInt((data.roomid) / 100000)][data.i].userid).emit("hitconfirm",data.j)
+    })
+
+    function checkdeath(roomid) {
+        let deathcount=0;
+        for (let i = 0; i < gameobj[parseInt(roomid / 100000)].length; i++) {
+            if (gameobj[parseInt(roomid / 100000)][i].hp <= 0) {
+                deathcount+=1;
+                io.to(gameobj[parseInt(roomid / 100000)][i].userid).emit("died")
+                io.to(roomid).emit("deadtank",i)
+            }
+        }
+        if(deathcount==((gameobj[parseInt(roomid / 100000)].length)-1)){
+            io.to(roomid).emit("win","Congratulations, You Won...")
+        }
+    }
+})
